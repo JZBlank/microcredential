2bfa8797d4b5a1c0b6f62b9cbe38dbb9334272be
JoyceZhang <56086743+JZBlank@users.noreply.github.com>
2022-08-09 15:19:29 -0400

    Add files via upload
    Updated checkOut page layout

--- project2_Zhang/checkOut/checkOut.js
+++ project2_Zhang/checkOut/checkOut.js
@@ -1,13 +1,13 @@
 
-display = document.querySelectorAll('.itemContainer .allItems > div')
+const display = document.querySelectorAll('.item')
 
 for(let index = 0; index < display.length; index++){
   if(sessionStorage.getItem(`itemName${index}`) != null){
-
+    console.log(display[index])
      itemName = document.querySelector(`#storedItem${index}`)
      itemPrice = document.querySelector(`#storedPrice${index}`)
 
-     display[index].style.display = 'block'
+     display[index].style.display = 'inline'
 
      itemName.innerHTML = sessionStorage.getItem(`itemName${index}`)
      itemPrice.innerHTML = "$"
@@ -18,14 +18,31 @@ for(let index = 0; index < display.length; index++){
 const clearCart = document.querySelector('.clearCart')
 const allInfo = document.querySelectorAll('.section > p')
 
+
+function clearCartItems(){
+  clearCartItems()
+  clearCartImages()
+}
+
 function clearCartItems(){
   for(let index = 0; index < allInfo.length ; index++){
     allInfo[index].innerHTML = ''
   }
 }
 
+function clearCartItems(){
+  for(let index = 0; index < display.length ; index++){
+    display[index].style.display = 'none'
+  }
+}
+
+
+
 clearCart.addEventListener('click', function(){
   sessionStorage.clear()
   clearCartItems()
 
 })
+
+// REMOVE ITEM FROM CART
+removeItem = document.querySelectorAll('.deleteItem')
